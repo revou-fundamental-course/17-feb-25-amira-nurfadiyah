d523d125272ccea4b2ff61ebde65ce9aa55f622a
amira <fadiyahnuramira@gmail.com>
2025-02-27 20:19:35 +0700

    update everything

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
 // Ini Javascript External 
+
+function validateForm() {
+    let nameInput = document.getElementById('name-input');
+    console.log(nameInput.value);
+
+    // Kondisi Validasinya
+    if(nameInput.value == '') { 
+        alert('Nama tidak boleh kosong');
+    } else {
+        alert('Welcome ' + nameInput.value);
+    }
+}
+
+// For Index Identifier
+let indexBanner = 0;
+
+showBanner();
+
+// Auto-slide every 3 sec
+setInterval(nextBanner, 3000)
+
+function nextBanner () {
+    // Increment IndexBanner
+    indexBanner += 1;
+    showBanner();
+}
+
+function showBanner() {
+    let banner = document.getElementsByClassName('main-banner');
+
+    // Reset Condition 
+    if (indexBanner > banner.length - 1) {
+        indexBanner = 0;
+    }
+
+    // Hide all banner
+    for (let i = 0; i < banner.length; i++) {
+        banner[i].style.display = "none"
+    }
+
+    //Show the current banner
+    banner[indexBanner].style.display = "block"
+}
